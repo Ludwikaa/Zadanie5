08cde2130559935046741549b9cbb298969da38b
Weronika Pijagin <pijaginw@ee.pw.edu.pl>
2024-04-07 19:34:34 +0200

    first commit

---
+++ src/zadanie5/Zadanie5.java
@@ -0,0 +1,15 @@
+package zadanie5;
+
+import java.util.Scanner;
+import
+
+public class Zadanie5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
